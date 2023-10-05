5f07bda42258a5f6d8407e27ed1fa6181ac303ac
rootboy1 <ravimulani101@gmail.com>
2023-10-05 09:38:44 -0400

    Day1

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,2 +1,16 @@
 name = input("Enter your name: ")
 print("Your name length is:", len(name))
+
+name1 = input("Enter First Name: ")
+name2 = input("Enter Secound Name: ")
+
+print("\nBefor Swapping")
+print("First Name: ", name1, "\nSecound Name: ", name2)
+
+#Swap two variables
+temp = name1
+name1 = name2
+name2 = temp
+
+print("\nAfter Swaaping")
+print("First Name: ", name1, "\nSecound Name: ", name2)
