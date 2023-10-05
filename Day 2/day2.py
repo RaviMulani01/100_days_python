5d69365cb9b2ffb1f4eab21320ebd95feb7a160e
rootboy1 <ravimulani101@gmail.com>
2023-10-05 11:34:48 -0400

    Day2

---
+++ Day 2/day2.py
@@ -0,0 +1,52 @@
+#Data Type
+
+#String
+"Hello"[0]
+"123" + "345"
+
+#Integer
+123 + 345
+#both same
+123456789
+123_456_789
+
+#Float
+3.14159
+
+#Boolean
+True 
+False
+
+#Sum of two individual number from two digit value
+# two_digit_number = input()
+
+# first_digit = int(two_digit_number[0])
+# secound_digit = int(two_digit_number[1])
+
+# sum_of_two_digits = first_digit+secound_digit
+
+# print(sum_of_two_digits)
+
+#Arithmatic Operation
+3 + 5  # 8
+7 - 4  # 3
+3 * 2  # 6
+6 / 3  # 2.0
+2 ** 3 # 8
+
+# PEMDASLR
+# ()
+# **
+# * /
+# + -
+
+#BMI Calculator
+
+height = float(input("Enter Your Height in m:"))
+
+weight = float(input("Enter Your Weight in kg:"))
+
+bmi = round(weight / (height * height),2)
+
+print("BMI index is: ", bmi)
+ 
