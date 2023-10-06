35ea1915971d0820739e9d1e488787cd22087f2d
rootboy1 <ravimulani101@gmail.com>
2023-10-06 11:36:24 -0400

    Da3 BMI calculator

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -15,16 +15,35 @@
 #     print("Odd number")
 
 # Nested If else
-height = int(input("Enter your height in cm? "))
+# height = int(input("Enter your height in cm? "))
 
-if height > 120:
-    print("You can ride")
-    age = int(input("Enter your age: "))
-    if age < 12:
-        print("Your Ticket Prive is $5")
-    elif age <= 18:
-        print("Your Ticket Prive is $7")
-    else:
-        print("Your ticker price is $12")
+# if height > 120:
+#     print("You can ride")
+#     age = int(input("Enter your age: "))
+#     if age < 12:
+#         print("Your Ticket Prive is $5")
+#     elif age <= 18:
+#         print("Your Ticket Prive is $7")
+#     else:
+#         print("Your ticker price is $12")
+# else:
+#     print("You can't ride")
+
+# BMI Calculator
+
+height = float(input("Enter Your Height in m:"))
+
+weight = float(input("Enter Your Weight in kg:"))
+
+bmi = weight / (height * height)
+
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi}, your are underweight.")
+elif bmi <= 25:
+     print(f"Your BMI is {bmi}, your have a normal weight.")
+elif bmi < 30:
+      print(f"Your BMI is {bmi}, your are slightly overweight.")
+elif bmi < 35:
+      print(f"Your BMI is {bmi}, your are obese.")
 else:
-    print("You can't ride")
+      print(f"Your BMI is {bmi}, your are clinically obese..")
