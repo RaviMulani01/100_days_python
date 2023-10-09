9b6d52fd926af7a21e207f269604abb2cd935ae8
rootboy1 <ravimulani101@gmail.com>
2023-10-09 16:54:53 -0400

    Day4 List

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -1,5 +1,30 @@
 import random
 
-# Genrate Random Number Beteen 0 to 4
-random_integer = random.randint(0,4)
-print(random_integer)
+# # Genrate Random Number Beteen 0 to 4
+# random_integer = random.randint(0,4)
+# print(random_integer)
+
+# # Genrate float point number. it will generate between 0.0000.. to 0.99999...
+# random_float = random.random()
+# print(random_float)
+
+# # Virtual toss program.
+# toss = random.randint(0,1)
+
+# if toss == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# List
+
+fruits = ["Apple", "Banana", "Orange"]
+print(fruits)
+
+# add item in end
+fruits.append("Mengo")
+print(fruits)
+
+#add item at specific index
+fruits.insert(1, "Pinapple")
+print(fruits)
