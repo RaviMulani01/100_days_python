3cff4e02e5db75870fe0094a08bedd9b3631e0fe
rootboy1 <ravimulani101@gmail.com>
2023-10-10 15:47:05 -0400

    Day 5 FizzBuzz

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -15,3 +15,19 @@
 # for i in range(2, target+1,2):
 #     sum += i
 # print(sum)
+
+# the FizzBuzz to print 1 to 100
+
+for i in range(1,101):
+
+    if i%3==0 and i%5==0:
+        print("FizzBuzz")
+    
+    elif i%3 == 0:
+        print("Fizz")
+
+    elif i%5 == 0:
+        print("Buzz")
+    
+    else:
+        print(i)
