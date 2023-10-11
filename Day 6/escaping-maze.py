ac104dda20b3342f638e95dfaa428bbf04da8a8f
rootboy1 <ravimulani101@gmail.com>
2023-10-11 16:05:04 -0400

    Day 6 Project

---
+++ Day 6/escaping-maze.py
@@ -0,0 +1,17 @@
+# For this project we are useing Reeborg,s Word Maze Task
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+
+#     elif front_is_clear():
+#         move()
+    
+#     else:
+#         turn_left()
