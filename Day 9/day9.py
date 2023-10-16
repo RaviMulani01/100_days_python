5cb05ddc91c98370becca9aada999f77cddcad0a
rootboy1 <ravimulani101@gmail.com>
2023-10-16 15:13:05 -0400

    Day 9 Dictionary in list

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -1,11 +1,11 @@
-# Dictionary
-college = { "name": "Unknown", "total_students": 101}
+# # Dictionary
+# college = { "name": "Unknown", "total_students": 101}
 
-# # Retrieving items from dictionary.
-# print(college["name"])
+# # # Retrieving items from dictionary.
+# # print(college["name"])
 
-# Adding new items to dictionary.
-college["Adress"] = "Toronto"
+# # Adding new items to dictionary.
+# college["Adress"] = "Toronto"
 
 # print(college)
 
@@ -23,31 +23,78 @@ college["Adress"] = "Toronto"
 # for things in college:
 #     print(things+ ":-", college[things])
 
-# Grading Program
-student_score = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62
-}
+# # Grading Program
+# student_score = {
+#     "Harry": 81,
+#     "Ron": 78,
+#     "Hermione": 99,
+#     "Draco": 74,
+#     "Neville": 62
+# }
 
-student_grades ={}
+# student_grades ={}
 
-for student in student_score:
-    score = student_score[student]
+# for student in student_score:
+#     score = student_score[student]
 
-    if score > 90:
-        student_grades[student] = "Outstanding"
+#     if score > 90:
+#         student_grades[student] = "Outstanding"
 
-    elif score > 80:
-        student_grades[student] = "Exceeds Expectation"
+#     elif score > 80:
+#         student_grades[student] = "Exceeds Expectation"
 
-    elif score >70:
-        student_grades[student] = "Acceptable"
+#     elif score >70:
+#         student_grades[student] = "Acceptable"
 
-    else:
-        student_grades[student] = "Fail"
+#     else:
+#         student_grades[student] = "Fail"
 
-for score in student_grades:
-    print(score,":",student_grades[score])
+# for score in student_grades:
+#     print(score,":",student_grades[score])
+
+# # Nesting
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin"
+# }
+
+# # Nesting a list in a Dictionary
+# travel_log = {
+#     "France": ["Paris", "Lille", "Dijion"],
+#     "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+# }
+
+# ["a", "b", ["c", "d"]]
+
+# # Nesting a dictionary in a Dictionary
+# total_visit = {
+#     "France":{
+#         "Paris": 2,
+#         "Lille": 4,
+#         "Dijion": 3
+#     }
+# }
+
+travel_log = [{
+    "country": "france",
+    "visits": 12,
+    "cities": ["paris", "lille", "Dijon"]
+    },
+
+    {
+    "country": "germany",
+    "visits": 5,
+    "cities": ["berlin", "hamburg", "stuttgart"]  
+    }
+]
+
+def add_new_country(country, visits, list_of_cities):
+    travel_log.append({"country": country, "visits": visits, "cities": list_of_cities})
+
+print("Before")
+print(travel_log)
+
+add_new_country("canada", 4, ["Toronto", "Brampton", "Mississauga"])
+
+print("After")
+print(travel_log)
