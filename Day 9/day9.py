ccef313e25bfa78ac385cf53dc4569b7ecf96305
rootboy1 <ravimulani101@gmail.com>
2023-10-16 11:51:25 -0400

    Day 9 dictionary

---
+++ Day 9/day9.py
@@ -0,0 +1,10 @@
+# Dictionary
+college = { "name": "Unknown", "total_students": 101}
+
+# Retrieving items from dictionary.
+print(college["name"])
+
+# Adding new items to dictionary.
+college["Adress"] = "Toronto"
+
+print(college)
