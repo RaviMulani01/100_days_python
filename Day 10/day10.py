5505be86cd0ee14be778b88f6a9cf1d6c54b11b2
rootboy1 <ravimulani101@gmail.com>
2023-10-17 10:26:40 -0400

    Day 10 function with return value

---
+++ Day 10/day10.py
@@ -0,0 +1,8 @@
+# # function with return value
+# def format_name(f_name,l_name):
+#     formeted_f_name = f_name.title()
+#     formeted_l_name = l_name.title()
+#     return f"{formeted_f_name} {formeted_l_name}"
+
+# name = format_name("ravi", "mulani")
+# print(name)
